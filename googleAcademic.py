83a25ca6bf393d53ef254bb73ea634020e6f438d
Antonio Vázquez Gutiérrez <avazquez62@alumnos.uaq.mx>
2020-10-20 19:57:20 -0500

    commit

--- googleAcademic.py
+++ googleAcademic.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 import time
+import os
 
 def findGoogle(search_param):
     #Ignorar los certificados:
